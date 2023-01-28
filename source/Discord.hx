b069cf69fc586bdeeeb21cb493ead0d9cdf92ddd
ayedFNFmakerSource code <123101780+ayedSourcecode@users.noreply.github.com>
2023-01-28 05:06:52 +0100

    Update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -42,10 +42,10 @@ class DiscordClient
 	static function onReady()
 	{
 		DiscordRpc.presence({
-			details: "In the Menus",
+			details: "Play Mods SlicerBeat",
 			state: null,
 			largeImageKey: 'icon',
-			largeImageText: "Psych Engine"
+			largeImageText: "Slicer beat"
 		});
 	}
 
@@ -82,7 +82,7 @@ class DiscordClient
 			details: details,
 			state: state,
 			largeImageKey: 'icon',
-			largeImageText: "Engine Version: " + MainMenuState.psychEngineVersion,
+			largeImageText: "Slicer Beat Version: " + MainMenuState.SlicerVersion,
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
 			startTimestamp : Std.int(startTimestamp / 1000),
